69e61c6bb7d3ae24597d1aa0bb81ff18587efe59
Beerunegi <birender.negi1@gmail.com>
2026-06-20 09:12:43 +0530

    update

--- tmp_test_fetch.js
+++ tmp_test_fetch.js
@@ -1,6 +1,25 @@
-const http = require('http');
-http.get('http://localhost:3000/sitemap.xml', res => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => console.log('DATA:', data));
-}).on('error', err => console.log('Error:', err.message));
+const urls = [
+  'https://digiwebtech.co.in/wp-blog/',
+  'https://digiwebtech.co.in/wp-blog/wp-login.php',
+  'https://digiwebtech.co.in/wp-blog/wp-json/wp/v2/posts'
+];
+
+async function test() {
+  for (const url of urls) {
+    try {
+      console.log('\n----------------------------');
+      console.log('Fetching:', url);
+      const res = await fetch(url);
+      console.log('Status:', res.status);
+      const headers = Object.fromEntries(res.headers.entries());
+      console.log('Server/Platform:', headers['server'], '/', headers['platform']);
+      console.log('Content-Type:', headers['content-type']);
+      const text = await res.text();
+      console.log('Snippet:', text.slice(0, 300));
+    } catch (e) {
+      console.error('Error fetching', url, ':', e.message);
+    }
+  }
+}
+
+test();
