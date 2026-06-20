997ddf3d4810063e8fa4d7667c8c572e6bc315d3
Beerunegi <birender.negi1@gmail.com>
2026-06-20 11:40:53 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   poweredByHeader: false,
   compress: true,
   trailingSlash: false,
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
   async redirects() {
     return [
       {
